b114f3f43a687ab1fa5173068355a5483b8bbff8
devBbmattieu <bbmattieu9@gmail.com>
2026-03-29 09:25:24 +0100

    feat(auth): implement JWT authentication, password hashing, and protected routes

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,38 @@
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
-SECRET_KEY = "your-secret-key"
+SECRET_KEY = "Matt@LAB98iii"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 security = HTTPBearer()
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = int(payload.get("sub"))
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    return user_id
--- app/main.py
+++ app/main.py
@@ -1,24 +1,40 @@
-from fastapi import FastAPI, HTTPException,status
+
+from fastapi import FastAPI, HTTPException, status, Depends
 from pydantic import BaseModel, EmailStr
 import random
 
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token,
+    get_current_user
+)
+
 
 app = FastAPI()
+
 
 users_db = []
 accounts_db = []
 transactions_db = []
 
-# Schemas
+# ===== SCHEMAS =====
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
 
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
 class AccountCreate(BaseModel):
     user_id: int
     account_type: str
     balance: float = 0.0
+
 
 class TransactionCreate(BaseModel):
     account_id: int
@@ -27,33 +43,14 @@ class TransactionCreate(BaseModel):
     description: str | None = None
 
 
-class UserLogin(BaseModel):
-    email: EmailStr
-    password: str
-
-
-# [GET] Root route
-@app.get("/")
-def root():
-    return {"message": "FinCore API running..."}
-
- ######### Helper funcs ###################
-# Helper func to find user
+# ========= HELPERS ===========
 def find_user(user_id: int):
     for user in users_db:
         if user["id"] == user_id:
             return user
     return None
 
-# Helper func to generate AcctNo
-def generate_account_number():
-    while True:
-        account_number = str(random.randint(100000, 999999))
-        if not any(acctNo["account_number"] == account_number for acctNo in accounts_db):
-            return account_number
 
-
-# Helper func to find Account
 def find_account(account_id: int):
     for account in accounts_db:
         if account["id"] == account_id:
@@ -61,31 +58,44 @@ def find_account(account_id: int):
     return None
 
 
-# Helper func to find Transaction by ID
 def find_transaction(tx_id: int):
-    for transaction in transactions_db:
-        if transaction["id"] == tx_id:
-            return transaction
+    for tx in transactions_db:
+        if tx["id"] == tx_id:
+            return tx
     return None
 
- ######### Enf of Helper funcs ###################
 
-# [POST] - Create User
+def generate_account_number():
+    while True:
+        account_number = str(random.randint(1000000000, 9999999999))
+        if not any(acc["account_number"] == account_number for acc in accounts_db):
+            return account_number
+
+
+# ======== ROUTES ========
+
+@app.get("/")
+def root():
+    return {"message": "FinCore API running..."}
+
+
+# -------- USERS --------
+
 @app.post("/users")
 def create_user(user: UserCreate):
     user_data = user.model_dump()
     user_data["id"] = len(users_db) + 1
+    user_data["password"] = hash_password(user.password)
+
     users_db.append(user_data)
     return user_data
 
 
-# [GET] Users
 @app.get("/users")
 def get_users():
     return users_db
 
 
-# [GET] User_by_id
 @app.get("/users/{user_id}")
 def get_user(user_id: int):
     user = find_user(user_id)
@@ -94,86 +104,81 @@ def get_user(user_id: int):
     return user
 
 
+# -------- AUTH --------
 
-# Accounts Endpoints -----------------------------------------
+@app.post("/login")
+def login(user: UserLogin):
+    for db_user in users_db:
+        if db_user["email"] == user.email:
+            if verify_password(user.password, db_user["password"]):
+                token = create_access_token({"sub": str(db_user["id"])})
+                return {"access_token": token, "token_type": "bearer"}
+
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
 
+# -------- ACCOUNTS --------
 
-# [POST] Create Account
 @app.post("/accounts")
-def create_account(account: AccountCreate):
-    # Validate User Exist
+def create_account(account: AccountCreate, current_user: dict = Depends(get_current_user)):
     user = find_user(account.user_id)
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User not found")
+
     account_data = account.model_dump()
     account_data["id"] = len(accounts_db) + 1
     account_data["account_number"] = generate_account_number()
+
     accounts_db.append(account_data)
     return account_data
 
 
-# [GET] Accounts
 @app.get("/accounts")
-def get_accounts():
+def get_accounts(current_user: dict = Depends(get_current_user)):
     return accounts_db
 
 
-# [GET] Account By ID
 @app.get("/accounts/{account_id}")
-def get_account(account_id: int):
+def get_account(account_id: int, current_user: dict = Depends(get_current_user)):
     account = find_account(account_id)
     if not account:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
     return account
 
 
-# Transactions Endpoints -----------------------------------------
+# -------- TRANSACTIONS --------
 
-# [GET] Perform a Transaction
 @app.post("/transactions")
-def create_transaction(tx: TransactionCreate):
+def create_transaction(tx: TransactionCreate, current_user: dict = Depends(get_current_user)):
     account = find_account(tx.account_id)
     if not account:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Account not found")
 
     if tx.type == "debit":
-        if tx["balance"] < tx.amount:
+        if account["balance"] < tx.amount:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient balance")
-        account["balance"] -= tx["amount"]
+        account["balance"] -= tx.amount
 
     elif tx.type == "credit":
-        account["balance"] += tx["amount"]
+        account["balance"] += tx.amount
 
     else:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid transaction type")
+
     tx_data = tx.model_dump()
     tx_data["id"] = len(transactions_db) + 1
+
     transactions_db.append(tx_data)
     return tx_data
 
-# [GET] Transactions
 @app.get("/transactions")
-def get_transactions():
+def get_transactions(current_user: dict = Depends(get_current_user)):
     return transactions_db
 
 
-
-# [GET] Transaction by ID
 @app.get("/transactions/{tx_id}")
-def get_transaction(tx_id: int):
-    transaction = find_transaction(tx_id)
-    if not transaction:
+def get_transaction(tx_id: int, current_user: dict = Depends(get_current_user)):
+    tx = find_transaction(tx_id)
+    if not tx:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Transaction not found")
-    return transaction
-
-
-# [POST]
-@app.post("/users")
-def create_user(user: UserCreate):
-    user_data = user.model_dump()
-    user_data["id"] = len(users_db) + 1
-    user_data["password"] = pwd_context.hash(user.password)
-
-    users_db.append(user_data)
-    return user_data
+    return tx
